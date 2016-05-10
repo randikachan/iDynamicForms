c9be575f83fedcbcf4a0d89842e14e6e53606002
Kasun Randika <kasun@gapstars.net>
2016-05-10 15:21:36 +0530

    Initial Commit

---
+++ iDynamicForms/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iDynamicForms
+//
+//  Created by Kasun Randika on 5/10/16.
+//  Copyright © 2016 Kasun Randika. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iDynamicForms/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iDynamicForms
+//
+//  Created by Kasun Randika on 5/10/16.
+//  Copyright © 2016 Kasun Randika. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
